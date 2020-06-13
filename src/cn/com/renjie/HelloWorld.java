e0708820d38a4db3ee642ff74ce47e94f7c3fdaf
renjie861129 <renjie!@#4>
2020-06-13 18:02:02 +0800

    Initial commit

---
+++ src/cn/com/renjie/HelloWorld.java
@@ -0,0 +1,20 @@
+package cn.com.renjie;
+
+import java.util.Arrays;
+
+/**
+ * @author Buddha.ren
+ * @create 2020-06-13 14:48
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("args = " + args);
+        System.out.println("HelloWorld.main");
+        System.out.println("args = " + Arrays.deepToString(args));
+
+
+
+    }
+}
